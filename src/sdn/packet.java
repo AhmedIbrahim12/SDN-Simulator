bb344502a1813aef9db58462b66e3f4bc5baeced
Ahmed Ibrahim <ahmed.mohamed.ibrahim.12@gmail.com>
2017-05-04 02:03:19 +0200

    creating packet class

---
+++ src/sdn/packet.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sdn;
+
+/**
+ *
+ * @author Ahmed Ibrahim
+ */
+public class packet {
+
+    String srcIP;
+    String destIP;
+    String data;
+
+    public packet(String sIP, String dIP, String msg) {
+        this.srcIP = sIP;
+        this.destIP = dIP;
+        this.data = msg;
+    }
+
+    public String getSrcIP() {
+        return this.srcIP;
+    }
+
+    public String getdestIP() {
+        return this.destIP;
+    }
+}
