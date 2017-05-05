9828dee1d046310b4dd6cc314cf538cbb61fdd35
sha3er94 <mohamedelsha3er@icloud.com>
2017-05-05 17:14:18 +0200

    Testing GUI Controller

--- src/sdn/SDN.java
+++ src/sdn/SDN.java
@@ -15,7 +15,8 @@ public class SDN {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        MainWindow t = new MainWindow();
+        t.setVisible(true);
     }
-    
+
 }
